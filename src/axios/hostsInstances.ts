564fd25e71c89f89190b8758fa4994d423c4da88
Andrés Ayala <aayalac@glingeriesas.com>
2024-05-05 09:39:15 -0500

    Change Gateway URL

--- src/axios/hostsInstances.ts
+++ src/axios/hostsInstances.ts
@@ -3,29 +3,27 @@ import axios, { AxiosInstance } from "axios";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 
-const HOST_URL = 'http://www.ambassadors.ink';
+const GATEWAY_URL = 'http://api.ambassadors.ink';
 export const CHECKOUT_WEB_APP_URL = 'http://checkout.ambassadors.ink';
 
-
-
 const authService = axios.create({
-  baseURL: `${HOST_URL}/auth/api/ambassador`,
+  baseURL: `${GATEWAY_URL}/auth/api/ambassador`,
   withCredentials: true
 });
 
 const registrationExperienceService = axios.create({
-  baseURL: `${HOST_URL}/registration-exp/api/users`,
+  baseURL: `${GATEWAY_URL}/registration-exp/api/users`,
   withCredentials: true
 });
 
 const usersService = axios.create({
-  baseURL: `${HOST_URL}/users/api/ambassador`,
+  baseURL: `${GATEWAY_URL}/users/api/ambassador`,
   withCredentials: true
 });
 setJWTToken(usersService);
 
 const coreService = axios.create({
-  baseURL: `${HOST_URL}/core/api/ambassador`,
+  baseURL: `${GATEWAY_URL}/core/api/ambassador`,
   withCredentials: true
 });
 setJWTToken(coreService);
